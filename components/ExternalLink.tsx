f27d33c79f0192bdaaf110b9b2aa9e39fb46cbf1
Jan Aagaard <jan@aagaard.net>
2026-05-17 23:08:10 +0200

    Remove web support

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,7 +1,6 @@
 import { Link } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
 import React from "react";
-import { Platform } from "react-native";
 
 export const ExternalLink = (
   props: Omit<React.ComponentProps<typeof Link>, "href"> & { href: string },
@@ -12,12 +11,10 @@ export const ExternalLink = (
       {...props}
       href={props.href}
       onPress={(e) => {
-        if (Platform.OS !== "web") {
-          // Prevent the default behavior of linking to the default browser on native.
-          e.preventDefault();
-          // Open the link in an in-app browser.
-          WebBrowser.openBrowserAsync(props.href as string);
-        }
+        // Prevent the default behavior of linking to the default browser on native.
+        e.preventDefault();
+        // Open the link in an in-app browser.
+        WebBrowser.openBrowserAsync(props.href as string);
       }}
     />
   );
