b60905f8447dc9dad5fb7a2352df836b5783213d
Jan Aagaard <jaa@zenegy.com>
2026-05-22 22:31:34 +0200

    Replace app.json with app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,39 @@
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "arbejdsglaede",
+  slug: "arbejdsglaede",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "arbejdsglaede",
+  userInterfaceStyle: "automatic",
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    predictiveBackGestureEnabled: false,
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+};
+
+export default config;
